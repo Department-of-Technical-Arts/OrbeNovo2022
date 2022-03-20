76b2a9c325f1ceb1c2c363936b1f19c9a36dca11
Aarush Sinha <sinhaaarush11@gmail.com>
2022-03-20 19:34:37 +0530

    changed the team detail

--- src/components/HomeComponents/Team/index.js
+++ src/components/HomeComponents/Team/index.js
@@ -4,7 +4,33 @@ import ScrollAnimation from "react-animate-on-scroll";
 
 import Title from "../../common/Text/Title";
 import President from "../../../assets/images/Group 55.png";
+import GenSec from "../../../assets/images/Group 66.png";
+import TechSec from "../../../assets/images/Group 65.png";
+import CulSecB from "../../../assets/images/Group 64.png";
+import CulSecG from "../../../assets/images/Group 63.png";
+
 function Index({ teamData }) {
+  const teamMember = (index) => {
+    switch (index) {
+      case 0:
+        return President;
+
+      case 1:
+        return GenSec;
+
+      case 2:
+        return TechSec;
+
+      case 3:
+        return CulSecB;
+
+      case 4:
+        return CulSecG;
+
+      default:
+        return null;
+    }
+  };
   return (
     <>
       <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" offset={150}>
@@ -14,21 +40,44 @@ function Index({ teamData }) {
           </div>
           <div className="col-span-1" />
           <div className="col-span-8">
-            <div className="grid grid-cols-6">
+            <div className="grid xl:hidden grid-cols-4">
+              {teamData.map((item, index) =>
+                index < 4 ? (
+                  <div
+                    key={index}
+                    className="col-span-2 flex flex-row justify-center 2xl:scale-100 lg:scale-75"
+                  >
+                    <img src={teamMember(index)} alt="" />
+                  </div>
+                ) : (
+                  <>
+                    <div className="col-span-1" />
+                    <div
+                      key={index}
+                      className="col-span-2 flex flex-row justify-center 2xl:scale-100 lg:scale-75"
+                    >
+                      <img src={teamMember(index)} alt="" />
+                    </div>
+                    <div className="col-span-1" />
+                  </>
+                )
+              )}
+            </div>
+            <div className="xl:grid hidden grid-cols-6">
               {teamData.map((item, index) =>
                 index < 3 ? (
                   <div
                     key={index}
                     className="col-span-3 xl:col-span-2 flex flex-row justify-center 2xl:scale-100 lg:scale-75"
                   >
-                    <img src={President} alt="" />
+                    <img src={teamMember(index)} alt="" />
                   </div>
                 ) : (
                   <div
                     key={index}
                     className="col-span-3 flex flex-row justify-center 2xl:scale-100 lg:scale-75"
                   >
-                    <img src={President} alt="" />
+                    <img src={teamMember(index)} alt="" />
                   </div>
                 )
               )}
