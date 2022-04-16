85b163f5590afd5dbdfa4fe670ebb54dbf615cd2
Sai Ankit <saiankit30@gmail.com>
2022-04-16 16:02:43 +0530

    add new spons

--- src/utils/SponsData.js
+++ src/utils/SponsData.js
@@ -1,32 +1,24 @@
 export const sponsData = [
   {
-    pic: "https://firebasestorage.googleapis.com/v0/b/orbenovo-22.appspot.com/o/Spons%2Famararaja.png?alt=media&token=924d8f89-701c-4297-8be8-8e66e160a945",
+    pic: require('../assets/images/ongc.png'),
   },
   {
-    pic: "https://firebasestorage.googleapis.com/v0/b/orbenovo-22.appspot.com/o/Spons%2Fjswsteel.png?alt=media&token=cdf2dcf2-8d33-4f6a-93dd-916436b93d7d",
+    pic: require('../assets/images/dukes.png'),
   },
   {
-    pic: "https://firebasestorage.googleapis.com/v0/b/orbenovo-22.appspot.com/o/Spons%2Fservicenow.png?alt=media&token=52aaed52-4813-4ded-9011-6e0990990361",
+    pic: require('../assets/images/samsung.png'),
+  },
+
+  {
+    pic: require('../assets/images/jain.png'),
   },
   {
-    pic: "https://firebasestorage.googleapis.com/v0/b/orbenovo-22.appspot.com/o/Spons%2Fcokestudio.png?alt=media&token=dd6c9a05-c90b-45ad-8963-e97764d09d35",
+    pic: require('../assets/images/dlt.png'),
   },
   {
-    pic: "https://firebasestorage.googleapis.com/v0/b/orbenovo-22.appspot.com/o/Spons%2Ftelanganatourism.png?alt=media&token=0efd5459-8412-4ad3-b46b-05d8ae4223d6",
+    pic: require('../assets/images/sbi.png'),
   },
   {
-    pic: "https://firebasestorage.googleapis.com/v0/b/orbenovo-22.appspot.com/o/Spons%2Fredfm.png?alt=media&token=0348057a-1a38-44a0-93ab-67962674ebd2",
-  },
-  {
-    pic: "https://firebasestorage.googleapis.com/v0/b/orbenovo-22.appspot.com/o/Spons%2Ftoi.png?alt=media&token=ab9037ed-74dd-4b38-8c1e-60013f09e7e4",
-  },
-  {
-    pic: "https://firebasestorage.googleapis.com/v0/b/orbenovo-22.appspot.com/o/Spons%2Fzoomcar.png?alt=media&token=71543a06-c6c4-4b03-9da2-bf9030df28c6",
-  },
-  {
-    pic: "https://firebasestorage.googleapis.com/v0/b/orbenovo-22.appspot.com/o/Spons%2Fbrtiishcouncil.png?alt=media&token=b121fdbc-5af5-41fb-84cb-5619a8478225",
-  },
-  {
-    pic: "https://firebasestorage.googleapis.com/v0/b/orbenovo-22.appspot.com/o/Spons%2Fsakshi.png?alt=media&token=e3b0a43b-3259-4e75-bbe4-55d45f19d330",
+    pic: require('../assets/images/mahindra.png'),
   },
 ];
